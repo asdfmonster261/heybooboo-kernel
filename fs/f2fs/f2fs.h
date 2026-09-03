2adbb00fc00bc400c5e860253e8e4fb24a788cc1
Bob Dole <77771419+asdfmonster261@users.noreply.github.com>
2026-09-02 23:44:50 -0500

    f2fs: shorten congestion wait to 6ms
    Retry sooner on IO congestion. From WildKernels/kernel_patches.

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -629,8 +629,8 @@ enum {
 
 #define MAX_FLUSH_RETRY_COUNT	3	/* maximum flush retry count in f2fs_enable_checkpoint() */
 
-/* congestion wait timeout value, default: 20ms */
-#define	DEFAULT_IO_TIMEOUT	(msecs_to_jiffies(20))
+/* congestion wait timeout value, default: 6ms */
+#define	DEFAULT_IO_TIMEOUT	(msecs_to_jiffies(6))
 
 /* timeout value injected, default: 1000ms */
 #define DEFAULT_FAULT_TIMEOUT	(msecs_to_jiffies(1000))
